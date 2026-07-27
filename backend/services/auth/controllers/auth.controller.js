50cdc4298a136562632ab1330130ccf6d4643a51
veeresh8055 <visesync@gmail.com>
2026-07-27 17:58:40 +0530

    authentication completed by using redis

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase-admin/auth";
 import User from "../models/user.model.js";
 import { app } from "../config/firebase.js";
+import redis from "../../../shared/redis/redis.js";
 
 export const login = async (req, res) => {
     try {
@@ -17,9 +18,29 @@ export const login = async (req, res) => {
             })
         }
         const sessionId = crypto.randomUUID();
+           
+        await redis.set(`session:-${sessionId}`, JSON.stringify({userId : user._id , name : user.name  , email:user.email , avatar : user.avatar }), 'EX', 7 * 24 * 60 * 60); // Store session for 7 days
+
+
         res.cookie("session", sessionId, { httpOnly: true, secure: false, sameSite: "strict", maxAge: 7 * 24 * 60 * 60 * 1000 });
     return  res.status(200).json( user );
     } catch (error) {
      return    res.status(500).json({ message: "Error occurred while logging in", error: error.message });
+    }
+}
+
+
+export const logout = async (req,res)=>{
+    try{
+        const sessionId = req.cookies.session;
+        if(sessionId){
+            await redis.del(`session:-${sessionId}`);
+            res.clearCookie("session");
+            return res.status(200).json({ message: "Logged out successfully" });
+        }else{
+            return res.status(400).json({ message: "No session found" });
+        }
+    }catch(error){
+        return res.status(500).json({ message: "Error occurred while logging out", error: error.message });
     }
 }
